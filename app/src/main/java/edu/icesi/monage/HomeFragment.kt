a925de1248c7daa1536ff4c54aff26876817c3e1
Camilo1909 <kmilo0919@gmail.com>
2022-04-28 18:58:29 -0500

    Home fragment

--- app/src/main/java/edu/icesi/monage/HomeFragment.kt
+++ app/src/main/java/edu/icesi/monage/HomeFragment.kt
@@ -19,14 +19,29 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(inflater,container,false)
 
-        binding.progressFood.setProgress(50,false)
+        binding.progressFood.setProgress(calculateProgress(true,false,false),true)
         binding.foodTxt.text = "${binding.progressFood.progress.toString()}${"%"}"
-        binding.progressHygiene.setProgress(100,false)
+        binding.progressHygiene.setProgress(calculateProgress(false,true,false),true)
         binding.hygieneTxt.text = "${binding.progressHygiene.progress.toString()}${"%"}"
-        binding.progressFun.setProgress(10,false)
+        binding.progressFun.setProgress(calculateProgress(false,false,true),true)
         binding.funTxt.text = "${binding.progressFun.progress.toString()}${"%"}"
 
         return binding.root
+    }
+
+    private fun calculateProgress(food:Boolean,hygiene: Boolean, funn: Boolean): Int{
+        var progress = 0
+        if (food){
+           //metodo del usuario para calcular
+            progress = 60
+        }else if (hygiene){
+            //metodo del usuario para calcular
+            progress = 100
+        }else{
+            //metodo del usuario para calcular
+            progress = 10
+        }
+        return progress
     }
 
     companion object {
