fbb4693739893e92d8963b8af60c57d1cdc1bfd1
Alex Samacá <alexadrito10@gmail.com>
2022-05-18 18:56:42 -0500

    Begining of Game class

---
+++ app/src/main/java/edu/icesi/monage/model/Game.kt
@@ -0,0 +1,26 @@
+package edu.icesi.monage.model
+
+class Game{
+    var days:Int = 0
+    var playersNum:Int = 0
+    var Scores:ArrayList<Int> = ArrayList()
+
+
+
+
+    fun 
+
+
+
+
+
+
+    fun NewDay(){
+        days = days+1
+    }
+
+
+
+
+
+}
